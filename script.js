e9714bb3989f7aef4340b369269b47fde2e7776f
Kacper <kacperdalgiewicz@gmail.com>
2021-11-15 23:16:43 +0100

    g

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ let kasa = 0;
 let koparka = 0;
 let palarnia = 0;
 let tyton = 0;
+let zalki = 0;
 let kosztUlepszenia1 = 50;
 let kosztUlepszenia2 = 100;
 let kosztUlepszenia3 = 200;
+let kosztUlepszenia4 = 300;
+let szacunekLvl2 = 10;
+let szacunekLvl3 = 20;
 const divSzacunek = document.getElementById("respect");
 const divHajs = document.getElementById("hajs");
 const divPerClick = document.getElementById("per_click");
@@ -25,6 +29,10 @@ const divUpgrade3 = document.getElementById("upgrade3");
 const upgrade3Face = document.getElementById("upgrade3-face");
 const divStat3 = document.getElementById("stat-tyton");
 const statTytonFace = document.getElementById("stat-tyton-face");
+const divUpgrade4 = document.getElementById("upgrade4");
+const upgrade4Face = document.getElementById("upgrade4-face");
+const divStat4 = document.getElementById("stat-zalki");
+const statZalkiFace = document.getElementById("stat-zalki-face");
 
 function klik() {
 	kasa += perKlik;
@@ -63,6 +71,14 @@ function upgrade3() {
 	kosztUlepszenia3 = parseInt(kosztUlepszenia3 * 1.5);
 }
 
+function upgrade4() {
+	kasa = kasa - kosztUlepszenia4;
+	perSecond += 3;
+	zalki += 1;
+	szacunek += 4;
+	kosztUlepszenia4 = parseInt(kosztUlepszenia4 * 1.5);
+}
+
 function wypisanie() {
 	divSzacunek.innerHTML = szacunek;
 	divHajs.innerHTML = kasa;
@@ -73,21 +89,27 @@ function wypisanie() {
 	divKoparka.innerHTML = koparka;
 	divKoszt2.innerHTML = kosztUlepszenia2;
 	divKoszt.innerHTML = kosztUlepszenia1;
-	if (szacunek >= 10) {
-		upgrade3Face.innerHTML = "Tytoń (+3 na kliknięcie)<br/>Koszt: <span id='koszt3'>" + kosztUlepszenia3 + "</span>";
+	if (szacunek >= szacunekLvl2) { //przypisywanie nowych ulepszen do rangi
+		upgrade3Face.innerHTML = "Tytoń (+3 na kliknięcie)<br/>Koszt: " + kosztUlepszenia3;
 		divUpgrade3.classList.remove("blocked");
-		statTytonFace.innerHTML = "Ile szlugów sprzedałeś:<br/><span id='tyton'>" + tyton + "</span>";
+		statTytonFace.innerHTML = "Ile szlugów sprzedałeś:<br/>" + tyton;
 		divStat3.classList.remove("blocked-stat");
+	}
+	if (szacunek >= szacunekLvl3) {
+		upgrade4Face.innerHTML = "Zalki (+3 na sekundę)<br/>Koszt: <span id='koszt4'>" + kosztUlepszenia4 + "</span>";
+		divUpgrade4.classList.remove("blocked");
+		statZalkiFace.innerHTML = "Ile zalek podrabiasz:<br/><span id='zalki'>" + zalki + "</span>";
+		divStat4.classList.remove("blocked-stat");
 	}
 }
 
-function ranga() {
-	if (szacunek >= 10) {
+function ranga() { //dodawanie rang
+	if (szacunek >= szacunekLvl2) {
 		divRanga.innerHTML = "Adept biologii";
 		opisRangi.innerHTML = "Poznajesz zasady hodowli super myszy i dowiadujesz się jak zasadzić świecący tytoń. Wykorzystaj dobrze tą wiedzę.";
 		image.style.backgroundImage = "url('img/adept_biologii.jpg')";
 	}
-	if (szacunek >= 20) {
+	if (szacunek >= szacunekLvl3) {
 		divRanga.innerHTML = "Douczony mistrzu";
 		opisRangi.innerHTML = "Coraz lepiej idzie ci zdobywanie kolejnych zalek. Znana fraza &quotDoucz się mistrzu&quot jest już dla ciebie tylko przeszłością.";
 		image.style.backgroundImage = "url('img/cool_kid.jpg')";
@@ -101,8 +123,11 @@ function upgrade(p) {
 	if (p == 2 && kasa >= kosztUlepszenia2) {
 		upgrade2();
 	}
-	if (p == 3 && szacunek >= 10 && kasa >= kosztUlepszenia3) {
+	if (p == 3 && szacunek >= szacunekLvl2 && kasa >= kosztUlepszenia3) {
 		upgrade3();
+	}
+	if (p == 4 && szacunek >= szacunekLvl3 && kasa >= kosztUlepszenia4) {
+		upgrade4();
 	}
 	wypisanie();
 	ranga();
